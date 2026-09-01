f2ebb178b0680cef04dddb8baef9bbbb0f30e202
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2026-09-01 16:11:09 -0400

    Fix comment consistency

--- src/event-listener-tracker.js
+++ src/event-listener-tracker.js
@@ -1,7 +1,7 @@
 // source: https://github.com/JamesRobertHugginsNgo/event-listener-tracker/blob/1.0.1/src/event-listener-tracker.js
 
-// ls-remote remote url: https://github.com/JamesRobertHugginsNgo/event-listener-tracker.git
-// ls-remote release branch: refs/heads/release/1
+// ls-remote repository: https://github.com/JamesRobertHugginsNgo/event-listener-tracker.git
+// ls-remote release: refs/heads/release/1
 // ls-remote release sha: d19ad5dc855afadfc826af8102eff5cbb88e9b45
 
 function normalizeOptions(options) {
